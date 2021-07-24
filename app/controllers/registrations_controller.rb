40528bd92b7b8dd2d6af59bbb3ae96952a038120
norbas <norbert.skumbin@gmail.com>
2021-07-24 15:18:50 +0200

    [fix] Disable token processing on user sign up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -16,6 +16,6 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:email, :password, :username)
+    params.require(:user).permit(:email, :first_name, :last_name, :password, :username)
   end
 end
