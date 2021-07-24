4c5aed1bca09b064e391eca46477da3635f98ac2
norbas <norbert.skumbin@gmail.com>
2021-07-24 15:17:30 +0200

    Fix doubled email uniqueness validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :username, uniqueness: true, presence: true
-  validates :email, uniqueness: true, presence: true
+  validates :email, presence: true
 
   def generate_jwt
     JWT.encode({id: id, exp: 14.days.from_now.to_i}, ENV['DEVISE_SECRET_KEY'])
