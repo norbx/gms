544d1f4cb2afd36e2b6520a8840664dc3c3a519c
norbas <norsku27@yahoo.com>
2022-03-23 20:39:13 +0100

    Add Tag model validations

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class Tag < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
+  before_validation { name.capitalize! }
+  validates :name, presence: true, uniqueness: true,
+                   format: { with: /\A[a-zA-Z]+-?[a-z]+\z/, message: 'only allows letters and single dashes' }
 
   has_and_belongs_to_many :bands
 end
