744c35d85f99bc7d0d52ae50b8c42f32f2b41243
norbas <norsku27@yahoo.com>
2022-03-26 17:17:15 +0100

    Remove name uniqueness validation from Band

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Band < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
 
   has_and_belongs_to_many :users
   has_and_belongs_to_many :tags
