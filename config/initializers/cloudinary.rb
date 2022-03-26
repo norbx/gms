2ea0887ddb72bb6c59cccc86eacb27537958e6aa
norbas <norsku27@yahoo.com>
2022-03-26 17:40:58 +0100

    Hide cloudinary credentials in env

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Cloudinary.config do |config|
-  config.cloud_name = 'codepirates'
-  config.api_key = '324182254615516'
-  config.api_secret = '8Ned0W1QKgyx1ZNMJtM9ruOI5xo'
+  config.cloud_name = ENV['CLOUDINARY_CLOUD_NAME']
+  config.api_key = ENV['CLOUDINARY_API_KEY']
+  config.api_secret = ENV['CLOUDINARY_API_SECRET']
   config.secure = true
   config.cdn_subdomain = true
 end
