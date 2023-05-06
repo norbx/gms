7443ac6006fb9cb04a88dfd50f1be8e1bca437b4
norbas <norsku27@yahoo.com>
2023-05-06 21:13:47 +0200

    Add seeds for bands and tags

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,30 @@
 # frozen_string_literal: true
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Tag.destroy_all
+Band.destroy_all
 
-User.find_or_create_by!(name: 'Diego', email: 'diego@mail.com', password_digest: BCrypt::Password.create('password'))
-User.find_or_create_by!(name: 'Ronnie', email: 'ronnie@mail.com', password_digest: BCrypt::Password.create('password'))
+diego = User.create(name: 'Diego', email: 'diego@mail.com', password_digest: BCrypt::Password.create("password"))
+ronnie = User.create(name: 'Ronnie', email: 'ronnie@mail.com', password_digest: BCrypt::Password.create("password"))
+
+Band.create(
+  name: "The Bongos", 
+  active: true,
+  contact_name: "Sir Gregor Clegane",
+  phone_number: "123456789",
+  description: "Kings of the bonks!",
+  social_links: { facebook: "www.facebook.com/thebongos" },
+  users: [diego],
+  tags: [Tag.create(name: "Disco"), Tag.create(name: "Wedding")]
+)
+
+Band.create(
+  name: "The Stonks",
+  active: true,
+  contact_name: "Sir Jorah Mormont",
+  phone_number: "987654321",
+  description: "We are The Stonks :)",
+  social_links: { twitter: "www.twitter.com/thestonks" },
+  users: [ronnie],
+  tags: [Tag.create(name: "Brit Pop"), Tag.create(name: "Rock")]
+)
